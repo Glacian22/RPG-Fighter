796edb244d6de232681c2865f75d44b65a7f6d1c
Glacian22 <47679905+Glacian22@users.noreply.github.com>
2019-03-05 19:18:19 -0800

    element spacing working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
-$(document).ready(function (){
+$(document).ready(function () {
 
     var gamestate = "start";
-    var player;
+
 
     //function to build character objects
     function character(name, HP, ATK, restImg) {
@@ -9,10 +9,31 @@ $(document).ready(function (){
         this.HP = HP;
         this.ATK = ATK;
         this.restImg = restImg;
-      }
+    }
 
-      //create character objects
-      var wizard = new character("wizard", 10, 6, "../images/wizard.jpg")
+    //create character objects
+    var wizard = new character("Wizard", 20, 60, "images/wizard.jpg");
+    var knight = new character("Knight", 40, 2, "images/knight.jpg");
+    var priest = new character("Priest", 6, 5, "images/priest.jpg");
+    var thief = new character("Knight", 15, 8, "images/thief.jpg");
+    var player="";
+
+    //start game code
+
+    $(".alert-field").html("Choose Your Character")
+
+    //set player character
+    $(".character").on("click", function () {
+        player=$(this).attr("name");
+        console.log("character is: ", player)
+
+        //set the player character
+        $(".alert-field").html("You are the " + player + "!");
+
+        //hide the player character to choose enemy
+        // $(".wizard").fadeOut();
+        $(this).hide()
+    });
 
 
 
@@ -27,5 +48,4 @@ $(document).ready(function (){
 
 
 
-
-})
+});
